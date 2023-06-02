078074c8da6010a3fb49cb7297e26f41e2018b75
Guus Veen <guus_veen@outlook.com>
2023-06-02 16:58:13 +0200

    Added final controllers

---
+++ FestivalBE/Controllers/ArtiestFavorietenController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FestivalBE.Models;
+
+namespace FestivalBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtiestFavorietenController : ControllerBase
+    {
+        private readonly FestiFactDbContext _context;
+
+        public ArtiestFavorietenController(FestiFactDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ArtiestFavorieten
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArtiestFavoriet>>> GetArtiestFavorieten()
+        {
+          if (_context.ArtiestFavorieten == null)
+          {
+              return NotFound();
+          }
+            return await _context.ArtiestFavorieten.ToListAsync();
+        }
+
+        // GET: api/ArtiestFavorieten/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ArtiestFavoriet>> GetArtiestFavoriet(int? id)
+        {
+          if (_context.ArtiestFavorieten == null)
+          {
+              return NotFound();
+          }
+            var artiestFavoriet = await _context.ArtiestFavorieten.FindAsync(id);
+
+            if (artiestFavoriet == null)
+            {
+                return NotFound();
+            }
+
+            return artiestFavoriet;
+        }
+
+        // PUT: api/ArtiestFavorieten/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArtiestFavoriet(int? id, ArtiestFavoriet artiestFavoriet)
+        {
+            if (id != artiestFavoriet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(artiestFavoriet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArtiestFavorietExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ArtiestFavorieten
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ArtiestFavoriet>> PostArtiestFavoriet(ArtiestFavoriet artiestFavoriet)
+        {
+          if (_context.ArtiestFavorieten == null)
+          {
+              return Problem("Entity set 'FestiFactDbContext.ArtiestFavorieten'  is null.");
+          }
+            _context.ArtiestFavorieten.Add(artiestFavoriet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArtiestFavoriet", new { id = artiestFavoriet.Id }, artiestFavoriet);
+        }
+
+        // DELETE: api/ArtiestFavorieten/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArtiestFavoriet(int? id)
+        {
+            if (_context.ArtiestFavorieten == null)
+            {
+                return NotFound();
+            }
+            var artiestFavoriet = await _context.ArtiestFavorieten.FindAsync(id);
+            if (artiestFavoriet == null)
+            {
+                return NotFound();
+            }
+
+            _context.ArtiestFavorieten.Remove(artiestFavoriet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArtiestFavorietExists(int? id)
+        {
+            return (_context.ArtiestFavorieten?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FestivalBE/Controllers/VoorstellingFavorietenController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FestivalBE.Models;
+
+namespace FestivalBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoorstellingFavorietenController : ControllerBase
+    {
+        private readonly FestiFactDbContext _context;
+
+        public VoorstellingFavorietenController(FestiFactDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VoorstellingFavorieten
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VoorstellingFavoriet>>> GetVoorstellingFavorieten()
+        {
+          if (_context.VoorstellingFavorieten == null)
+          {
+              return NotFound();
+          }
+            return await _context.VoorstellingFavorieten.ToListAsync();
+        }
+
+        // GET: api/VoorstellingFavorieten/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VoorstellingFavoriet>> GetVoorstellingFavoriet(int? id)
+        {
+          if (_context.VoorstellingFavorieten == null)
+          {
+              return NotFound();
+          }
+            var voorstellingFavoriet = await _context.VoorstellingFavorieten.FindAsync(id);
+
+            if (voorstellingFavoriet == null)
+            {
+                return NotFound();
+            }
+
+            return voorstellingFavoriet;
+        }
+
+        // PUT: api/VoorstellingFavorieten/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVoorstellingFavoriet(int? id, VoorstellingFavoriet voorstellingFavoriet)
+        {
+            if (id != voorstellingFavoriet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(voorstellingFavoriet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VoorstellingFavorietExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/VoorstellingFavorieten
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<VoorstellingFavoriet>> PostVoorstellingFavoriet(VoorstellingFavoriet voorstellingFavoriet)
+        {
+          if (_context.VoorstellingFavorieten == null)
+          {
+              return Problem("Entity set 'FestiFactDbContext.VoorstellingFavorieten'  is null.");
+          }
+            _context.VoorstellingFavorieten.Add(voorstellingFavoriet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVoorstellingFavoriet", new { id = voorstellingFavoriet.Id }, voorstellingFavoriet);
+        }
+
+        // DELETE: api/VoorstellingFavorieten/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVoorstellingFavoriet(int? id)
+        {
+            if (_context.VoorstellingFavorieten == null)
+            {
+                return NotFound();
+            }
+            var voorstellingFavoriet = await _context.VoorstellingFavorieten.FindAsync(id);
+            if (voorstellingFavoriet == null)
+            {
+                return NotFound();
+            }
+
+            _context.VoorstellingFavorieten.Remove(voorstellingFavoriet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VoorstellingFavorietExists(int? id)
+        {
+            return (_context.VoorstellingFavorieten?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
